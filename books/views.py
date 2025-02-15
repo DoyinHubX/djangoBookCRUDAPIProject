f39c27b80728f434d9cc6dcfb2b4de83eb003f5e
Doyin <doyinelugbadebo@gmail.com>
2025-02-15 12:55:33 +0100

    improve API response clarity with custom messages

--- books/views.py
+++ books/views.py
@@ -1,13 +1,49 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.response import Response
 
 class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()  # Retrieve all books from the database
     serializer_class = BookSerializer  # Use the BookSerializer for serialization
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(
+            {
+                "message": "Book created successfully!",
+                "book": serializer.data
+            },
+            status=status.HTTP_201_CREATED
+        )
+
+
 class BookRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Book.objects.all()  # Retrieve all books from the database
-    serializer_class = BookSerializer  # Use the BookSerializer for serialization
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(
+            {
+                "message": "Book updated successfully!",
+                "book": serializer.data
+            },
+            status=status.HTTP_200_OK
+        )
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            {
+                "message": "Book deleted successfully!"
+            },
+            status=status.HTTP_204_NO_CONTENT
+        )
